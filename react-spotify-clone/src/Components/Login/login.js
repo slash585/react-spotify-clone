88e07f9eee092c394e41fcc00617522ab3477fa6
slash585 <51036040+slash585@users.noreply.github.com>
2020-10-18 01:38:23 +0300

    spotify.js docs complated

--- react-spotify-clone/src/Components/Login/login.js
+++ react-spotify-clone/src/Components/Login/login.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './login.css'
+import {accessUrl} from '../../spotify'
 
 function Login() {
     return (
         <div className="login">
             <h2>Im login page</h2>
             <img src="https://ambientmusicguide.com/wp-content/uploads/2018/11/spotify-banner.png" alt="spotify-logo" />            
-            <a>LOGIN WITH SPOTIFY</a>
+            <a href={accessUrl}>LOGIN WITH SPOTIFY</a>
 
         </div>
     )
