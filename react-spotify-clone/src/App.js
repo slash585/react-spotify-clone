b9d1dec4d6794a0cfdc4558a74a2816cf883c342
slash585 <51036040+slash585@users.noreply.github.com>
2020-10-19 00:43:57 +0300

    Spotify web api imported and user queries created

--- react-spotify-clone/src/App.js
+++ react-spotify-clone/src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Login from './Components/Login/login'
+import Login from './Components/Login/login';
+import {getTokenFromResponse} from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token,setToken] = useState(null);
+
+  useEffect(()=>{
+    const hash = getTokenFromResponse();
+    window.location.hash ="";
+
+    let _token = hash.access_token;
+
+    if(_token){
+      setToken(_token)
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user =>{
+        console.log('Kullanıcı:',user)
+      });
+
+    }
+    console.log("i have a token",token)
+  },[]);
+
+
   return (
     <div className="app">
       
+      {
+        token ? (
+          <h1>Hoşgeldiniz</h1>
+        ):
+        (
+          <Login/>
+        )
+      }
 
       
-      <Login/>
 
 
     </div>
